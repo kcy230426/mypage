92b6cd4af060f13b0d03f998aba0310f0c011d6e
kcy230426 <purple_galaxy@naver.com>
2023-12-12 19:28:44 +0900

    푸터 작업

--- project/src/component/Footer.js
+++ project/src/component/Footer.js
@@ -1,9 +1,14 @@
 import '../scss/my.scss'
+import ftlogo from '../asset/ft_logo.svg'
 
 const Footer = ()=>{
     return(
-        <footer>
-            하단
+        <footer id="ft">
+            <ul>
+                <li class="logo"><img src={ftlogo} alt="하단 로고" /></li>
+                <li class="content1">Thank for your Reading!</li>
+                <li class="content2">Copyright 2023. Kim chaeyeon’s Portfolio</li>
+            </ul>
         </footer>
     )
 }
