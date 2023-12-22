a31c57048e2fb1ebf50c0e4d9d0b90e661f41af2
kcy230426 <purple_galaxy@naver.com>
2023-12-22 14:56:43 +0900

    usestate를 이용해 이미지 - 썸네일 연결

--- project/src/component/Template.js
+++ project/src/component/Template.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import '../scss/my.scss'
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
@@ -5,6 +6,7 @@ import 'swiper/swiper-bundle.css';
 import 'swiper/css/pagination'
 import 'swiper/css/autoplay'
 import { Navigation, Pagination } from 'swiper/modules';
+import img1 from '../asset/sign1.png'
 
 const Template = (props)=>{
     const data = props.dbtads
@@ -17,6 +19,19 @@ const Template = (props)=>{
         modules: [Pagination, Navigation],
         navigation: true,
       };
+
+      const [selectedImage, setSelectedImage] = useState(img1);
+
+      const [selectedAlt, setSelectedAlt] = useState('썸네일1');
+
+      const handleThumbnailClick = (newImage) => {
+        setSelectedImage(newImage); // 선택된 이미지를 변경
+      };
+
+      const handleThumbnailAltClick = (newImage) => {
+        setSelectedImage(newImage); // 선택된 이미지를 변경
+      };
+
     return(
         <section id="template">
                 {data.tbar.map((v,i)=>(
@@ -30,10 +45,10 @@ const Template = (props)=>{
                 ))}
             <ul id="display">
                             <li className='show'>
-                                <Swiper className="swiper" spaceBetween={0} slidesPerView={1} pagination={{ type:'fraction', clickable: true }} autoplay={{ delay: 8000, disableOnInteraction: false,}} modules={[Pagination, Navigation]} navigation={true}>
+                                <Swiper className="swiper" spaceBetween={0} slidesPerView={1} pagination={{ type:'fraction', clickable: true }} autoplay={{ delay: 8000, disableOnInteraction: false,}} modules={[Pagination]}>
                                     {data.tads.map((v, i)=>(
                                         <SwiperSlide key={v.id}>
-                                            <img className={v.imgcls} src={v.img} alt={v.imgalt} />
+                                            <img className={v.imgcls} src={selectedImage} alt={selectedAlt} />
                                         </SwiperSlide>
                                     ))}
                                 </Swiper>
@@ -42,7 +57,7 @@ const Template = (props)=>{
                                 <Swiper {...twoswiper} className="swiper">
                                         {data.tads.map((v, i)=>(
                                             <SwiperSlide key={v.id}>
-                                                <img className={v.thumbcls} src={v.thumb} alt={v.thumbalt} />
+                                                <img className={v.thumbcls} src={v.thumb} alt={v.thumbalt} onClick={() => handleThumbnailClick(v.img, v.alt)} />
                                             </SwiperSlide>
                                         ))}
                                 </Swiper>
